87f4c24495e3ff1d8204f0607fe6cfd6646b5284
lenny126 <lennyariasq@gmail.com>
2017-08-17 22:18:58 -0500

    asdasd

--- resources/views/persona/vertodo.blade.php
+++ resources/views/persona/vertodo.blade.php
@@ -3,10 +3,34 @@
 @section('descripciontitulo','listado de personas') 
 @section('cuerpogeneral')
 <b style="color:red"><h1><center><i> vista desde el controlador </i></center></h1></b>
-<br>
-<br>
-@foreach($listaPersona as $key => $value)
-	<div><b>{{$value->nombre}}</b></div>
 
-@endforeach
+
+	<table class="table table-striped"> 
+		<thead>
+			<th>Nombre</th>
+			<th>Apellido</th>
+			<th>DNI</th>
+			<th>sexo</th>
+			<th>fecha de nacimiento</th>
+			<th>Correo electronico </th>
+			<th>fecha de registro</th>
+		</thead>
+		<tbody>
+			@foreach($listaPersona as $key => $value)
+			<tr>
+				<td>{{$value->nombre}}</td>
+				<td>{{$value->apellido}}</td>
+				<td>{{$value->dni}}</td>
+				<td>{{($value->sexo ? 'masculino' : 'femenino')}}</td>
+				<td>{{$value->fechanacimiento}}</td>
+				<td>{{$value->correoelectronico}}</td>
+				<td>{{$value->created_at}}</td>
+
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	
+
+
 @endsection 
